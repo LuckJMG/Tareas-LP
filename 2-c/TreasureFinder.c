8ff98dd5a0aa433e077bb8b9dbb3a4a61d251e4b
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2023-08-29 11:36:25 -0400

    refactor(1-c): comentar TreasureFinder.c

--- 2-c/TreasureFinder.c
+++ 2-c/TreasureFinder.c
@@ -7,11 +7,15 @@
 
 int main(int argc, char const *argv[])
 {
+    // Generar semilla random con el tiempo actual
     srand(time(NULL));
+
+    // Inicio de juego
     printf("¡Bienvenido a TreasureFinder!\n");
     printf("Indique el tamaño del tablero a jugar:\n");
     printf("1.7x7 2.10x10 3.12x12\n");
 
+    // Selección del tamaño del tablero
     int option;
     printf("Su input: ");
     scanf("%d", &option);
@@ -28,19 +32,20 @@ int main(int argc, char const *argv[])
     case 3:
         IniciarTablero(12);
         break;
-
-    default:
-        break;
     }
 
+    // Loop de juego
     int turno = 1;
     printf("Empezando juego... ¡listo!\n");
     printf("Tablero (Turno %d)\n", turno);
     MostrarTablero();
     while (1)
     {
+        // Acabar juego al encontrar todos los tesoros
         if (cantidad_tesoros == tesoros_encontrados)
             break;
+
+        // Acción del turno
         printf("Seleccione una acción:\n");
         printf("1.Colocar Bomba 2.Mostrar Bombas 3.Mostrar Tesoros\n");
         printf("Escoja una opcion: ");
@@ -48,15 +53,19 @@ int main(int argc, char const *argv[])
         switch (option)
         {
         case 1:
+            // Colocar nueva bomba
             Bomba *bomba = (Bomba *)malloc(sizeof(Bomba));
             int fila;
             int columna;
 
+            // Coordenadas Bombas
             printf("Indique las coordenadas de la bomba\n");
             printf("Fila: ");
             scanf("%d", &fila);
             printf("Columna: ");
             scanf("%d", &columna);
+
+            // Explosión Bomba
             printf("Indique forma en que explota la bomba\n");
             printf("1.Punto 2.X\n");
             printf("Su input: ");
@@ -72,12 +81,11 @@ int main(int argc, char const *argv[])
                 bomba->contador_turnos = 3;
                 bomba->explotar = &ExplosionX;
                 break;
-
-            default:
-                break;
             }
 
             ColocarBomba(bomba, fila - 1, columna - 1);
+
+            // Siguiente turno
             turno++;
             PasarTurno();
             printf("\nTablero (Turno %d)\n", turno);
@@ -91,13 +99,10 @@ int main(int argc, char const *argv[])
         case 3:
             VerTesoros();
             break;
-
-        default:
-            break;
         }
     }
+
+    // Finalización del juego, limpieza de memoria final
     printf("Felicidades, encontraste todos los tesoros!!!\n");
     BorrarTablero();
-
-    return 0;
 }
